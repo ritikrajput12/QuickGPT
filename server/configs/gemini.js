fc07f58d07f89de3b482ac124129a6537b98201c
Ritik <ritikrajput543678@gmail.com>
2026-02-03 03:15:42 +0530

    switch to official gemini sdk

--- server/configs/gemini.js
+++ server/configs/gemini.js
@@ -2,8 +2,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash"
-})
-
-export default model
+export default genAI
